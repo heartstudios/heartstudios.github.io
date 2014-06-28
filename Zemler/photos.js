95160e352d7a79633ba967ed778638ca87b86ab8
Heart Studios <heartstudios@user.noreply.github.com>
2014-06-28 15:20:16 -0700

    Add everything from the live site.

---
+++ Zemler/photos.js
@@ -0,0 +1,64 @@
+WPGgallery = new Object();
+WPGgallery.name = "Edited%20Engagement%20Photos";
+WPGgallery.photographer = "Diana%20Blazick";
+WPGgallery.contact = "Click%20thumbnails%20to%20view%20high%20resolution%20files.";
+WPGgallery.email = "diana@heartstudios.com";
+WPGgallery.date = "12/29/07"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "Christy%20and%20Danny%20final.jpg";
+gPhotos[0].ImageWidth = 1300;
+gPhotos[0].ImageHeight = 866;
+gPhotos[0].ThumbWidth = 200;
+gPhotos[0].ThumbHeight = 133;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "IMG_7610.jpg";
+gPhotos[1].ImageWidth = 866;
+gPhotos[1].ImageHeight = 1300;
+gPhotos[1].ThumbWidth = 133;
+gPhotos[1].ThumbHeight = 200;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "IMG_7630.jpg";
+gPhotos[2].ImageWidth = 866;
+gPhotos[2].ImageHeight = 1300;
+gPhotos[2].ThumbWidth = 133;
+gPhotos[2].ThumbHeight = 200;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "IMG_7650e.jpg";
+gPhotos[3].ImageWidth = 866;
+gPhotos[3].ImageHeight = 1300;
+gPhotos[3].ThumbWidth = 133;
+gPhotos[3].ThumbHeight = 200;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "IMG_7709.jpg";
+gPhotos[4].ImageWidth = 840;
+gPhotos[4].ImageHeight = 1300;
+gPhotos[4].ThumbWidth = 129;
+gPhotos[4].ThumbHeight = 200;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "IMG_7770.jpg";
+gPhotos[5].ImageWidth = 836;
+gPhotos[5].ImageHeight = 1300;
+gPhotos[5].ThumbWidth = 128;
+gPhotos[5].ThumbHeight = 200;
+gPhotos[5].meta = new Object();
+
